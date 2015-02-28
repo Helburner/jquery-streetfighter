a8ef9b34e862f154f05dfa4b53c490ee2130a62b
Helburner <Helburner@users.noreply.github.com>
2015-02-28 18:33:28 +0200

    finished Ryu animations

--- js/app.js
+++ js/app.js
@@ -6,6 +6,47 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').finish().show();
+		$('.hadouken').show().animate(
+			{'left': '1120px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '600px');
+			});
+
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		
 	});
 
+
 });
+
+$(document).ready().keydown(function(e) {
+	if (e.keyCode == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-cool').show();
+	}
+})
+
+.keyup(function(e) {
+	if (e.keyCode == 88) {
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	}
+});
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
+
